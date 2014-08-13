f648530b65bbdaf4e4c8178119be9912610276ee
JORGE <kanh_2832@hotmail.com>
2014-08-12 19:26:51 -0500

    Bombas Aleatorias v1.0
    Bombas Aleatorias v1.0

---
+++ Bombas Aleatorias/bomba.js
@@ -0,0 +1,39 @@
+var tablero = [[ 0, 0, 0, 0],
+			   [ 0, 0, 0, 0],
+			   [ 0, 0, 0, 0],
+			   [ 0, 0, 0, 0]
+			  ];
+
+function generateNumber (min, max) 
+{
+	var value = Math.floor(Math.random() * (max - min + 1) + min);
+	return value;	
+}
+
+function cambiarValPosicion () 
+{	var marca = 0;
+	do
+	{	
+		var x = generateNumber(0, 3);
+		var y = generateNumber(0, 3);
+		if (tablero[x][y] == 0) 
+		{
+			tablero[x][y] = 1;
+			marca += 1;
+		}
+
+	}while(marca < 8);
+}
+
+cambiarValPosicion();
+
+function imprimir () {
+	for (var i = 0; i < 4; i++) {
+		for(var j = 0; j < 4; j++){
+			document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+tablero[i][j]+ "   ");
+		}
+		document.write("<br/>");
+	}
+}
+
+
